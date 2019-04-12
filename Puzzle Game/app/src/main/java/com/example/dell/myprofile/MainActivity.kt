464273700cd1324367ff70fb9a9b914bd525d188
Gourav Sharma <32862786+gouravsharmag@users.noreply.github.com>
2019-04-12 09:15:52 -0700

    Add files via upload

---
+++ Puzzle Game/app/src/main/java/com/example/dell/myprofile/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.dell.myprofile
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.FragmentManager
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    private val logTag = "MainFragment"
+
+    val fragmentManager: FragmentManager = supportFragmentManager
+
+    var n = 3
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(logTag, "onCreate")
+        setContentView(R.layout.activity_main)
+        showMainFragment()
+    }
+
+    private fun showMainFragment() {
+        val transaction = fragmentManager.beginTransaction()
+        val fragment = MainFragment()
+        transaction.replace(R.id.fragment_holder, fragment)
+        transaction.commit()
+    }
+
+    fun showPuzzleFragment(newN: Int) {
+        n = newN
+        val transaction = fragmentManager.beginTransaction()
+        val fragment = PuzzleFragment()
+        transaction.replace(R.id.fragment_holder, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+}
+
